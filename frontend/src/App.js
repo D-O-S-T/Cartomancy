36f7301d71158668be0399e46e0ef0fa21b3fa7e
Alex Silva <desufg826@gmail.com>
2025-05-03 16:55:49 -0300

    CRUD user funcionando

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import GlobalStyle from "./styles/global";
 import styled from "styled-components";
 import Form from "./components/Form.js";
-import Grid from "./components/Grid.js";
-import { ToastContainer} from "react-toastify";
+import Grid from "./components/Grid";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 
 const Container = styled.div`
-  width: 100%
+  width: 100%;
   max-width: 800px;
   margin-top: 20px;
   display: flex;
@@ -15,17 +17,33 @@ const Container = styled.div`
   gap: 10px;
 `;
 
-const Title = styled.h2` `;
+const Title = styled.h2``;
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [onEdit, setOnEdit] = useState(null);
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("http://localhost:8800");
+      setUsers(res.data.sort((a, b) => (a.nome > b.nome ? 1 : -1)));
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, [setUsers]);
+
   return (
     <>
       <Container>
         <Title>USUÁRIOS</Title>
-        <Form />
-        <Grid />
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
       </Container>
-      <ToastContainer position="bottom-left" autoClose={3000} />
+      <ToastContainer autoClose={3000} position="bottom-left" />
       <GlobalStyle />
     </>
   );
--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -41,29 +41,55 @@ export const Td = styled.td`
     }
 `;
 
-const Grid = ({ users }) => {
+const Grid = ({ users, setUsers, setOnEdit }) => {
+    const handleEdit = (item) => {
+      setOnEdit(item);
+    };
+  
+    const handleDelete = async (id) => {
+      await axios
+        .delete("http://localhost:8800/" + id)
+        .then(({ data }) => {
+          const newArray = users.filter((user) => user.id !== id);
+  
+          setUsers(newArray);
+          toast.success(data);
+        })
+        .catch(({ data }) => toast.error(data));
+  
+      setOnEdit(null);
+    };
+  
     return (
-        <Table>
-            <Thead>
-                <Tr>
-                    <Th>Nome</Th>
-                    <Th>Email</Th>
-                    <Th onlyWeb>Telefone</Th>
-                    <Th></Th>
-                    <Th></Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                {users.map((item, i) => (
-                    <Tr key={1}>
-                        <Td width="30%">{item.nome}</Td>
-                        <Td width="30%">{item.email}</Td>
-                        <Td width="20%" onlyWeb>{item.fone}</Td>
-                    </Tr>
-                ))}
-            </Tbody>
-        </Table>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Nome</Th>
+            <Th>Email</Th>
+            <Th onlyWeb>Fone</Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {users.map((item, i) => (
+            <Tr key={i}>
+              <Td width="30%">{item.nome}</Td>
+              <Td width="30%">{item.email}</Td>
+              <Td width="20%" onlyWeb>
+                {item.fone}
+              </Td>
+              <Td alignCenter width="5%">
+                <FaEdit onClick={() => handleEdit(item)} />
+              </Td>
+              <Td alignCenter width="5%">
+                <FaTrash onClick={() => handleDelete(item.id)} />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     );
-};
+  };
 
 export default Grid;
