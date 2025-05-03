d9b8275919fee722fb75e465bea04804b45c5e0e
Alex Silva <desufg826@gmail.com>
2025-05-02 22:59:56 -0300

    Primeiros testes de conexão

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import userRoutes from "./routes/users.js" //Importando as rotas de usuários
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json())
+app.use(cors()) //Evitar conflitos de acesso rodando localmente
+
+app.use("/", userRoutes)
 
 app.listen(8800);
