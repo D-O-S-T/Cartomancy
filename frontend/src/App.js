2d1b638697562316b408491020505c284133ffbd
Alex Silva <desufg826@gmail.com>
2025-05-03 13:55:58 -0300

    Adição de páginas para interação com o banco

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,33 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid.js";
+import { ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Container = styled.div`
+  width: 100%
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2` `;
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form />
+        <Grid />
+      </Container>
+      <ToastContainer position="bottom-left" autoClose={3000} />
+      <GlobalStyle />
+    </>
   );
 }
 
