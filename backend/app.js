cc199f5e70b7514f371076aee317204bfd52404e
Ana <anapaularubyany@gmail.com>
2025-06-16 13:27:20 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
+const path = require('path');
+const userRoutes = require('./routes/usuarioRoutes');
 const swaggerSetup = require('./config/swagger');
 const trilhaRoutes = require('./routes/trilhaRoutes');
 const anotacaoRoutes = require('./routes/anotacaoRoutes');
 const cartaRoutes = require('./routes/cartaRoutes');
+const videoRoutes = require('./routes/videoRoutes');
+const pdfRoutes = require('./routes/pdfRoutes');
+const materiaisRoutes = require('./routes/materiaisRoutes');
+
+
 
 const app = express();
 
@@ -17,8 +23,19 @@ app.use('/api/trilhas', trilhaRoutes);
 app.use('/api/usuarios', userRoutes);
 app.use('/api/anotacoes', anotacaoRoutes);
 app.use('/api/cartas', cartaRoutes);
+app.use('/videos', videoRoutes);
+app.use('/pdfs', pdfRoutes);
+app.use('/materiais', materiaisRoutes);
 
 // Swagger
 swaggerSetup(app);
+
+// Middleware para servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota padrão para servir o index.html (opcional, mas recomendado)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'teste-api.html'));
+});
 
 module.exports = app;
